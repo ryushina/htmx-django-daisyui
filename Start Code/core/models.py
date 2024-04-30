f49e55a90ef405b09475aadb113be4fbd8808930
Rustan <rustanlacanilao@gmail.com>
2024-05-01 07:07:37 +0800

    tets

--- Start Code/core/models.py
+++ Start Code/core/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
+# Register your models here.
 class User(AbstractUser):
     pass
+
+
+class Todo(models.Model):
+    description = models.CharField(max_length=255)
