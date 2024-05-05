b98bed44b7fcf6d90cccb9b2bc160f528a6eb00e
Rustan <rustanlacanilao@gmail.com>
2024-05-05 09:21:48 +0800

    created new requirements.txt

--- Start Code/core/views.py
+++ Start Code/core/views.py
@@ -8,3 +8,14 @@ from core.forms import TodoForm
 def index(request):
     context = {"todos": Todo.objects.filter(user=request.user), "form": TodoForm()}
     return render(request, "index.html", context)
+
+
+@login_required
+def submit_todo(request):
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        todo = form.save(commit=False)
+        todo.user = request.user
+        todo.save()
+
+        # return an html partial
